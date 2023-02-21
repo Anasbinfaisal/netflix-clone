b18962b708e8b88736f018587f075287e28144a7
Anas Bin Faisal <anasbinfaisal@gmail.com>
2023-02-21 19:24:41 +0500

    updated env

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const uri = process.env["MONGODB_URI"];
+const uri = process.env.MONGODB_URI;
 
 mongoose
   // .connect("mongodb://127.0.0.1:27017/netflix", {
