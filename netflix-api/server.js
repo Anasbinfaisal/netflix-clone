8f8dd26cdfc2535bee82dc5b409fa71a5c379867
Anas Bin Faisal <anasbinfaisal@gmail.com>
2023-02-21 15:29:29 +0500

    updated env

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("../netflix-api/routes/UserRoutes");
-
+require("dotenv").config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = process.env["MONGODB_URI"];
+
 mongoose
   // .connect("mongodb://127.0.0.1:27017/netflix", {
-  .connect(!process.env.MONGODB_URI, {
+  .connect(uri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
